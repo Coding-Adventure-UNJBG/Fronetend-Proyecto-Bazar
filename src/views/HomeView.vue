7e1f32ec30e2d9698911dea6f01f446161d8ee2b
José Luis <jlhuancac@unjbg.edu.pe>
2023-05-24 02:08:38 -0500

    Modificando imagen bazar-home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
       <div class="row d-flex justify-content-center align-items-center">
         <div class="col-lg-9 gradient-custom-2">
                 <div class="card text-center ">
-                  <img src="../assets/bazar-home.jpg" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;"/>
+                  <img src="../assets/bazar-home.png" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;"/>
                   <div class="card-body">
                     <button class="btn btn-danger">Dev-Team</button>
                   </div>
