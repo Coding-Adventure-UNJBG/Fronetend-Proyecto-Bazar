5cb7fbbf3a48ecbc77ceeeeebfff916ed5a6640f
DennisCa-UNJBG <dscalderonm@unjbg.edu.pe>
2023-05-28 12:04:27 -0500

    Subiendo la vista de reportes

--- src/views/ReportesView.vue
+++ src/views/ReportesView.vue
@@ -1,49 +1,96 @@
+<script setup>
+  import Navegacion from '../components/Navegacion.vue'
+  import { onMounted, ref } from 'vue';
+  import { useRouter } from 'vue-router';
 
-<script>
-import { ref } from 'vue';
-import Modal from '../components/Modal.vue';
-import Navegacion from '../components/Navegacion.vue'
-
-export default {
-  components: {
-    Modal,
-    Navegacion
-  },
-  setup() {
-    const modalVisible = ref(false);
-    const modalTitle = ref('Título del Modal');
-    const modalData = ref('Datos para el Modal');
-
-    const openModal = () => {
-      modalVisible.value = true;
-    };
-
-    return {
-      modalVisible,
-      modalTitle,
-      modalData,
-      openModal
-    };
-  }
-};
+  const router = useRouter()
 </script>
 <template>
   <Navegacion />
-  <div class="about">
-    <h1>Vista de Entrada de productos</h1>
-  <div>
-    <!-- Uso del componente Modal -->
-    <button @click="openModal">Abrir Modal</button>
-    <Modal :title="modalTitle" :value="modalVisible" :data="modalData" @update:value="modalVisible = $event" />
-  </div>
+
+  <div class="container-fluid d-flex justify-content-center align-items-center">
+    <div class="w-personalizado mx-auto">
+
+      <div class="card shadow m-4">
+
+        <!-- Cabezera -->
+        <div class="card-header py-3">
+          <div class="row g-3">
+
+          <h1 class="text-center custom-tittle">VISTA DE REPORTES</h1>
+
+          </div>
+        </div>
+        <!-- Fin cabezera -->
+        <!-- Cuerpo card -->
+        <div class="card-body">
+          <div class="row d-flex justify-content-center align-items-center text-center">
+            <div class="col-md-4">
+                <h5 class="custom-tittle">Usuarios</h5>
+            </div>
+            <div class="col-md-8">
+                <button type="button" class="btn btn-primary custom-btn-color" >
+                  <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+                  Generar
+                </button>
+            </div>
+            <div class="col-md-4">
+                <h5 class="custom-tittle">Productos</h5>
+            </div>
+            <div class="col-md-8">
+                <button type="button" class="btn btn-primary custom-btn-color" >
+                  <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+                  Generar
+                </button>
+            </div>
+            <div class="col-md-4">
+                <h5 class="custom-tittle">Compras</h5>
+            </div>
+            <div class="col-md-8">
+              <input type="date" class="btn"/>
+              <span> a </span>
+              <input type="date" class="btn"/>
+                <button type="button" class="btn btn-primary custom-btn-color" >
+                  <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+                  Generar
+                </button>
+            </div>
+            <div class="col-md-4">
+                <h5 class="custom-tittle">Ventas</h5>
+            </div>
+            <div class="col-md-8">
+              <input type="date" class="btn"/>
+              <span> a </span>
+              <input type="date" class="btn"/>
+                <button type="button" class="btn btn-primary custom-btn-color" >
+                  <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+                  Generar
+                </button>
+            </div>
+          </div>
+        </div>
+        <!-- Fin cuerpo card -->
+      </div>
+    </div>
   </div>
 </template>
 
+<style scoped>
 
-<style>
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+
+
+
+/* Ancho completo en la vista móvil */
+@media (max-width: 768px) {
+  .w-personalizado {
+    width: 100%;
   }
+}
+
+/* Ancho al 75% para vista no movil */
+@media (min-width: 769px) {
+  .w-personalizado {
+    width: 75%;
+  }
+}
 </style>
