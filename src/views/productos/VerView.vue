1edd9b1f67c08db1a9784b461eccbeb592d0c0ca
oscarchoq <oachoques@unjbg.edu.pe>
2023-05-31 15:14:09 -0500

    Se agrego una validación de ID en anteriorProducto

--- src/views/productos/VerView.vue
+++ src/views/productos/VerView.vue
@@ -49,10 +49,17 @@ function siguienteProducto() {
 }
 
 function anteriorProducto() {
-  const pId = parseInt(router.params.id) - 1
-  router.params.id = parseInt(router.params.id) - 1;
-  cargarData()
-  rutas.replace({ name: 'productover', params: { id: pId } })
+  // const pId = parseInt(router.params.id) - 1
+  // router.params.id = parseInt(router.params.id) - 1;
+  // cargarData()
+  // rutas.replace({ name: 'productover', params: { id: pId } })
+  const tempId = parseInt(router.params.id);
+  if (tempId > 1) {
+    const pId = tempId - 1;
+    router.params.id = pId;
+    cargarData();
+    rutas.replace({ name: 'productover', params: { id: pId } })
+  }
 }
 
 </script>
@@ -60,25 +67,25 @@ function anteriorProducto() {
 <template>
   <Navegacion />
 
-<div class="container-fluid d-flex justify-content-center align-items-center">
-  <div class="w-personalizado mx-auto">
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
-    <div class="card shadow" style="margin-bottom: 4%;">
-      <div class="row g-0">
-        <div class="col-xl-7">
-          <div class="card-body">
-            <h3 class="mb-4 mt-4 text-center custom-tittle">Vista de los Datos del Producto: {{ $route.params.id }}
-            </h3>
-            <form>
-              <div class="mb-3">
-                <label for="formNombre" class="form-label custom-tittle">Nombre</label>
-                <input type="text" class="form-control" id="formNombre" v-model="dataProductos.nombre" disabled>
-              </div>
+  <div class="container-fluid d-flex justify-content-center align-items-center">
+    <div class="w-personalizado mx-auto">
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+      <div class="card shadow" style="margin-bottom: 4%;">
+        <div class="row g-0">
+          <div class="col-xl-7">
+            <div class="card-body">
+              <h3 class="mb-4 mt-4 text-center custom-tittle">Vista de los Datos del Producto: {{ $route.params.id }}
+              </h3>
+              <form>
                 <div class="mb-3">
-                  <label for="formMarca" class="form-label custom-tittle">Marca</label>
-                  <input type="text" class="form-control" id="formMarca" v-model="dataProductos.marca" disabled>
+                  <label for="formNombre" class="form-label custom-tittle">Nombre</label>
+                  <input type="text" class="form-control" id="formNombre" v-model="dataProductos.nombre" disabled>
                 </div>
                 <div class="mb-3">
+                  <label for="formMarca" class="form-label custom-tittle">Marca</label>
+                <input type="text" class="form-control" id="formMarca" v-model="dataProductos.marca" disabled>
+              </div>
+              <div class="mb-3">
                   <label for="formMedida" class="form-label custom-tittle">Medida</label>
                   <input type="text" class="form-control" id="formMedida" v-model="dataProductos.medida" disabled>
                 </div>
@@ -122,12 +129,22 @@ function anteriorProducto() {
                 <img v-bind:src="dataProductos.foto" alt="" class="img-fluid img-thumbnail image-custom" />
 
               </div>
-              
+
               <div class="d-flex justify-content-center pt-4 mb-2">
-                <button type="button" class="btn btn-primary me-3" @click="anteriorProducto">
-                  <font-awesome-icon :icon="['fas', 'circle-left']" />&nbsp;&nbsp;Anterior</button>
-                <button type="button" class="btn btn-primary" @click="siguienteProducto">Siguiente&nbsp;
-                  <font-awesome-icon :icon="['fas', 'circle-right']" /></button>
+                <div @keydown.left="anteriorProducto" @keydown.right="siguienteProducto">
+                  <button type="button" class="btn btn-primary me-3" @click="anteriorProducto">
+                    <font-awesome-icon :icon="['fas', 'circle-left']" />&nbsp;&nbsp;Anterior
+                  </button>
+                  <button type="button" class="btn btn-primary" @click="siguienteProducto">
+                    Siguiente&nbsp;
+                    <font-awesome-icon :icon="['fas', 'circle-right']" />
+                  </button>
+                </div>
+
+                <!-- <button type="button" class="btn btn-primary me-3" @click="anteriorProducto">
+                    <font-awesome-icon :icon="['fas', 'circle-left']" />&nbsp;&nbsp;Anterior</button>
+                  <button type="button" class="btn btn-primary" @click="siguienteProducto">Siguiente&nbsp;
+                    <font-awesome-icon :icon="['fas', 'circle-right']" /></button> -->
               </div>
             </div>
 
@@ -136,7 +153,6 @@ function anteriorProducto() {
       </div>
     </div>
   </div>
-
 </template>
 
 <style>
