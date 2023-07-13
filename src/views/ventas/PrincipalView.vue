ae4dc49e369bf121aeea1c247315ed51feea1ed7
oscarchoq <oachoques@unjbg.edu.pe>
2023-07-13 12:39:08 -0500

    View Venta
    - Conectando la BD con el componente principal de ventas

--- src/views/ventas/PrincipalView.vue
+++ src/views/ventas/PrincipalView.vue
@@ -1,8 +1,24 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import Navegacion from '../../components/Navegacion.vue'
+import { ref } from 'vue';
 
 const router = useRouter()
+const dataVenta = ref('')
+
+async function cargarData() {
+  await fetch(`${import.meta.env.VITE_API_V1}/venta`, {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(dataVenta.value)
+      if (data.hasOwnProperty("error")) {
+        dataVenta.value = ''
+      } else
+        dataVenta.value = data
+    })
+}
 
 function nuevaVenta() {
   router.push({ name: 'ventanueva' })
@@ -43,9 +59,9 @@ function nuevaVenta() {
                   <thead>
                     <tr class="text-center align-middle">
                       <th width="4%" scope="col" class="fw-bold">N°</th>
-                      <th width="16%" class="fw-bold">NOMBRE</th>
-                      <th width="10%" class="fw-bold">FECHA</th>
+                      <th width="16%" class="fw-bold">BOLETA</th>
                       <th width="10%" class="fw-bold">CANTIDAD</th>
+                      <th width="10%" class="fw-bold">FECHA</th>
                       <th width="10%" class="fw-bold">IMPORTE</th>
                       <th width="10%" class="fw-bold">PAGO</th>
                       <th width="30%" class="fw-bold">COMENTARIO</th>
@@ -53,48 +69,17 @@ function nuevaVenta() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr class="text-center align-middle">
-                      <td>1</td>
-                      <td>Jabol Antibacterial</td>
-                      <td>20/05/2023</td>
-                      <td>5</td>
-                      <td>80.00</td>
-                      <td>Efectivo</td>
-                      <td></td>
-                      <td>
-                        <a href="#" data-toggle="tooltip" title="Ver Factura"><img alt="Vue logo" class="logo"
-                            src="@/assets/icons/ojo.svg" width="15" /></a>
-                      </td>
-                      <td>
-                        <a href="#" data-toggle="tooltip" title="Editar"><img alt="Vue logo" class="logo"
-                            src="@/assets/icons/pencil.svg" width="15" /></a>
-                      </td>
+                    <tr v-if="dataVenta == ''">
+                      <td colspan="100%">No se encontraron resultados</td>
                     </tr>
-                    <tr class="text-center align-middle">
-                      <td>2</td>
-                      <td>Jabol Antibacterial</td>
-                      <td>20/05/2023</td>
-                      <td>5</td>
-                      <td>80.00</td>
-                      <td>Efectivo</td>
-                      <td></td>
-                      <td>
-                        <a href="#" data-toggle="tooltip" title="Ver Factura"><img alt="Vue logo" class="logo"
-                            src="@/assets/icons/ojo.svg" width="15" /></a>
-                      </td>
-                      <td>
-                        <a href="#" data-toggle="tooltip" title="Editar"><img alt="Vue logo" class="logo"
-                            src="@/assets/icons/pencil.svg" width="15" /></a>
-                      </td>
-                    </tr>
-                    <tr class="text-center align-middle">
-                      <td>3</td>
-                      <td>Jabol Antibacterial</td>
-                      <td>20/05/2023</td>
-                      <td>5</td>
-                      <td>80.00</td>
-                      <td>Yape</td>
-                      <td></td>
+                    <tr v-else class="text-center align-middle" v-for="venta in dataVenta" :key="dataVenta.id">
+                      <td>{{ venta.id_venta }}</td>
+                      <td>{{ venta.serie }} - {{ venta.correlativo }}</td>
+                      <td>0</td>
+                      <td>{{ venta.fecha }}</td>
+                      <td>{{ venta.total_dinero }}</td>
+                      <td>{{ venta.tipo_pago }}</td>
+                      <td>{{ venta.comentario }}</td>
                       <td>
                         <a href="#" data-toggle="tooltip" title="Ver Factura"><img alt="Vue logo" class="logo"
                             src="@/assets/icons/ojo.svg" width="15" /></a>
